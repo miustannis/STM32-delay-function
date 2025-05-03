0cb5822951b4ca3c19ea2d0a8d61964753862647
Ye Miao <99935915+miustannis@users.noreply.github.com>
2025-05-03 16:36:15 +0800

    Update delay.h

--- delay.h
+++ delay.h
@@ -1,13 +1,16 @@
 #include "main.h"
 
-// 获取当前CPU周期数（直接读取寄存器）
 #define DWT_GetCycles()    DWT->CYCCNT
 
-uint8_t delay_ms(uint16_t time, uint32_t* get_tick);
+uint8_t Delay_ms_uwTick(uint16_t time, uint32_t* get_tick);
+
+void Delay_ms_uwTick_cycle(uint16_t time, uint32_t* get_tick);
 
 void DWT_Init(void) ;
 
-void Delay_us(uint32_t us);
+void Delay_us_cpu(uint32_t us);
+
+void Delay_ms_cpu(uint32_t ms);
 
 uint8_t Timer_Check_us(uint32_t *last_cycles, uint32_t interval_us);
 
